560c4df29fe941af62c2606a5b11cabb04598414
Diane Uwamariya <dianahporter@gmail.com>
2026-06-23 16:02:42 +0200

    feat: My first middleware

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ app.get('/users:id', (req,res) => res.json({id: req.params.id}))
 // GET /search?q=node&page=2 
 app.get('/search', (req,res)=> {
     const {q,page = '1'} = req.query;
-    req.json({query: q, page: Number(page)});
+    res.json({query: q, page: Number(page)});
 })
+
+//middleware example
+
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
 
 app.listen(3000, ()=>{
     console.log('Listening on http://localhost:3000'); 
